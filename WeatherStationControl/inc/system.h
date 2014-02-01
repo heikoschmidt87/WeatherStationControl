5fcccb8709720b912249bbd76ea2e8b803b0c87d
Heiko Schmidt <heiko.schmidt87@gmx.de>
2014-02-01 22:55:52 +0100

    initial commit

---
+++ WeatherStationControl/inc/system.h
@@ -0,0 +1,13 @@
+/*
+ * system.h
+ *
+ *  Created on: 01.02.2014
+ *      Author: heiko
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+
+
+#endif /* SYSTEM_H_ */
---
+++ WeatherStationControl/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 01.02.2014
+ *      Author: heiko
+ */
+
+int main(void) {
+
+	while(1) {
+
+	}
+
+	return 0;
+}
